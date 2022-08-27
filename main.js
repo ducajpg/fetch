0476e6c7ed6983deccde241dc501e341f07a9780
duca <holaduca@gmail.com>
2022-08-27 14:42:08 -0300

    lkj

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+let botonTerror = addEventListener("click", function() {
+    Swal.fire({
+        title: 'Get out',
+        text: 'Un joven va por primera vez a la casa de sus futuros suegros, una serie de descubrimientos hacen ese encuentro inquietante',
+        imageUrl: 'https://pics.filmaffinity.com/D_jame_salir-290175782-mmed.jpg',
+        imageWidth: 200,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+      })
+});
+
+
